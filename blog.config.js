46b4bd2421c35df18726a4f281857fef28b0f10d
Joe <ethan.zcm@gmail.com>
2025-07-09 23:08:49 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Joe的出海笔记', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '外贸从业者｜互联网工具爱好者｜实用主义者｜拒绝被割韭菜的探索者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://joenotes.co', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '出海笔记,外贸教程,外贸开发,博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Joe的出海笔记,外贸教程,外贸工具分享,外贸客户开发,博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
